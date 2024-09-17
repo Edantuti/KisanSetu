eabe6fd03a4df9fcf46ea8c8b1a5d2b0edd37c24
Anshuman Rai <118046194+anshuman-rai-27@users.noreply.github.com>
2024-09-17 12:55:46 +0530

    login_ui

--- app/dashboard/contract/create/page.tsx
+++ app/dashboard/contract/create/page.tsx
@@ -4,8 +4,8 @@ import { redirect } from "next/navigation";
 
 export default async function Page() {
   return (
-    <section className="min-h-[40rem] flex flex-col gap-2">
-      <h1 className="text-xl font-semibold">Contract Form</h1>
+    <section className="min-h-[40rem] w-[80vw] mx-[10vw] flex flex-col gap-2">
+      {/* <h1 className="text-xl font-semibold">Contract Form</h1> */}
       <ContractForm />
     </section>
   );
